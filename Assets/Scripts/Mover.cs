d9cf351feab4fa28915a341ac11f527ad83648dc
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-08-21 13:35:33 -0700

    changing animations

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }        
+    }
+
+    void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+}
